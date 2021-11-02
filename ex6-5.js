ba7bb83a4f4a3ac4919fa43e8f3a6582c102fa1e
zm20537 <zm20537@josai.ac.jp>
2021-11-02 01:06:23 +0000

    11.2

--- ex6-5.js
+++ ex6-5.js
@@ -2,10 +2,12 @@ var who65 = 'world';
 function sayhello65(){
     alert('Hello,' + who65 + '!');
 }
-
+var kita = who65 + 'が来た';
 function taro65(){
     who65 = '太郎';
+    alert(kita);
 }
 function hanako65(){
     who65 = '花子';
+    alert(kita);
 }

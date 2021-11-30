ac994b9b7a003c9b12e91059fd7dfa14cd7de553
zm20537 <zm20537@josai.ac.jp>
2021-11-30 01:36:58 +0000

    11.30

--- ex6-10.js
+++ ex6-10.js
@@ -2,5 +2,5 @@ function imgchange1(){
     document.getElementById('logo').src='./image/nyancat.gif';
 }
 function imgchange2(){
-    document.getElementById('logo2').src='./image/keiei_small.jpg';
+    document.getElementById('logo').src='./image/keiei_small.jpg';
 }

39afeabef9903ec0904d7c93032699d182c44790
zm20537 <zm20537@josai.ac.jp>
2021-12-21 01:16:18 +0000

    12.21

--- ex6-12.js
+++ ex6-12.js
@@ -1,19 +1,21 @@
 function hyoji()
 {
     //表示する文字
+    //strは、文字列
     var str ="一文字ずつ表示します。";
     //テキストボックスの文字数
-    var cnt = document.timer.moji.value.length;
+    var cnt = document.timert.mojimo.value.length;
     //文字が全部表示されているか確認
     if ( cnt < 11 )
     {
         //現在より1文字多く切り出して表示
-        document.timer.moji.value = str.substr(0,cnt+1);
+        //document＝html, timer＝htmlで名前つけたやつ, moji＝テキストボックスにつけた名前
+        document.timert.mojimo.value = str.substr(0,cnt+1);
     }
     else
     {
         //全て表示されたら、空文字に戻す
-        document.timer.moji.value= "";
+        document.timert.mojimo.value= "";
     }
     setTimeout("hyoji()",1000);
 }

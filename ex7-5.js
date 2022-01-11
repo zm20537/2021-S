3a0938bb0bdf0ac96c64d05158847e9bd665918a
zm20537 <zm20537@josai.ac.jp>
2022-01-11 01:30:32 +0000

    1.11

--- ex7-5.js
+++ ex7-5.js
@@ -3,5 +3,9 @@ function nazo()
     if(document.getElementById('answer').value=='夢')
     {
             alert('あたり！');
+            else
+            {
+                alert('はずれ');
+            }
     }
 }

86c7c0d0ec4551565048de0c4499bc2fba8f217d
zm20537 <zm20537@josai.ac.jp>
2022-01-11 01:51:39 +0000

    1.11

--- ex7-8.js
+++ ex7-8.js
@@ -1,8 +1,17 @@
 function nazozo(){
-    if(document.getElementById('answer').value='夢'){
+    if(document.getElementById('answer2').value=='夢')
+    {
         alert('あたり！');
     }
-    else{
+    else if(document.getElementById('answer2').value=='ゆめ')
+    {
+        alert('あたり！');
+    }
+    else
+    {
         alert('はずれ！');
     }
 }
+
+//value='夢'にしているためex7-5では、==のイコール（等しい）という意味だったが
+//それが代入に変わったから。
